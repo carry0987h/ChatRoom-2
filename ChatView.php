83ebfcb04acf296dc7a5190bf478d1d8b165fc28
rbretmounet <rbretmounet@gmail.com>
2018-04-19 22:02:40 -0700

    Split chat post into another file and made it refresh chat

--- ChatView.php
+++ ChatView.php
@@ -2,7 +2,6 @@
 <html>
 <head>
   <meta http-equiv="refresh" content="1" />
-  <link rel="stylesheet" type="text/css" href="ChatRoom.css">
   <title>ChatRoom</title>
   <script src="jquery-3.3.1.js"></script>
   <script src="ChatBox.js"></script>
