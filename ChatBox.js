0ea428348a0a24d30cd1eb61c2152160d05e481e
rbretmounet <rbretmounet@gmail.com>
2018-04-20 10:25:21 -0700

    Added enter for submit

--- ChatBox.js
+++ ChatBox.js
@@ -1,20 +1,5 @@
-
-function updateDisplay(url){
-  var request = new XMLHttpRequest();
-  request.open('GET', url);
-  request.responseType = 'text';
-  request.onload = function(){
-    document.getElementById("ChatView").textContent = request.response;
-  };
-  request.send();
-  setInterval(updateDisplay(url), 1000);
-};
-
-
 function checkSubmit(e){
   if(e && e.keyCode == 13){
     document.forms[0].submit();
   }
 };
-
-updateDisplay("http://"+window.location.hostname + "/ChatView.php");
