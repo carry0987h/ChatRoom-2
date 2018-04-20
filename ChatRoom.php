9c5e5a554c12d0634c53e6f038e7497690605db9
rbretmounet <rbretmounet@gmail.com>
2018-04-20 11:11:44 -0700

    Added jquery library

--- ChatRoom.php
+++ ChatRoom.php
@@ -9,10 +9,15 @@
 
 <body>
   <?php
+  //Creates variable for $cookin_name called user
   $cookie_name = "user";
+  //When there is a $cookie_name defined then it starts code.
   if(!isset($_COOKIE[$cookie_name])) {
+    //creates a varialbe called url to random name generator api.
     $url = "https://uinames.com/api/";
+    //creates a variable called obj to get the api.
     $obj = json_decode(file_get_contents($url), true);
+    //sets $cookie_value to name of api
     $cookie_value = $obj['name'];
     setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
   }else{
@@ -21,11 +26,11 @@
   }
   ?>
   <?php
-    if ($_SERVER['REQUEST_METHOD'] == 'POST'){
       //creates variable to open userlog.
       $handle = fopen("/tmp/userLog.txt", 'a+');
       //writes text iputed in form to userlog.txt
       fwrite($handle, $cookie_value. ": ". $_POST[posting]. "\n");
+      //closes userLog.txt
       fclose($handle);
   }
   ?>
