8fc0900def4b920e926253f969bf52cffe9bbdf2
rbretmounet <rbretmounet@gmail.com>
2018-04-20 10:32:01 -0700

    Added jquery library

--- ChatBox.js
+++ ChatBox.js
@@ -1,4 +1,5 @@
-$('ChatView').load(document.URL +  'ChatView');
+$('ChatView').load(document.URL +  'ChatView.php');
+
 function checkSubmit(e){
   if(e && e.keyCode == 13){
     document.forms[0].submit();
