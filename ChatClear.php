fcd498407e8b3c67bc7ba71651d5fb4f759c400f
rbretmounet <rbretmounet@gmail.com>
2018-05-15 10:15:25 -0700

    Added a button to clear chat log which can be later used to do by by default

--- ChatClear.php
+++ ChatClear.php
@@ -2,7 +2,7 @@
       //creates variable to open userlog.
       $handle = fopen("/tmp/userLog.txt", 'w');
       //writes text iputed in form to userlog.txt
-      fwrite($handle, " ");
+      fwrite($handle, "k");
       //closes userLog.txt
       fclose($handle);
 ?>
