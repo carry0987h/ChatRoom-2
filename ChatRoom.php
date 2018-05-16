723a8e56c27e5780a1df93b1f0e92badcacf0677
staceymay <missstaceymay@gmail.com>
2018-05-15 21:20:49 -0700

    Trying to get chatview to only render instead of refresh

--- ChatRoom.php
+++ ChatRoom.php
@@ -8,6 +8,15 @@
 </head>
 
 <body>
+<?php
+function renderHtml($contentRenderer = null)
+{
+    echo "<!DOCTYPE HTML>\n";
+    echo '<html>';
+    if ($contentRenderer) $contentRenderer();
+    echo '</html>';
+}
+?>
   <?php
   //Creates variable for $cookie_name called user
   $cookie_name = "user";
