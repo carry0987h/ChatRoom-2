3a03d8320ed79c6c526c178e60c53118fb427864
staceymay <missstaceymay@gmail.com>
2018-05-19 09:17:41 -0700

    Add instructions

--- ChatRoom.php
+++ ChatRoom.php
@@ -38,12 +38,14 @@
     <iframe src="/ChatView.php" width=400 height=400 frameborder=0 id="innerframe"></iframe>
   </div>
   <div class="userBox" onKeyPress="return checkSubmit(event)" id="Box">
+    <div class="container">
     <form action="/ChatLog.php" method="post">
       <textarea id="box" name="posting"></textarea>
       <p>
         Please type in your response here and hit enter.
       </p>
     </form>
+    </div>
   </div>
   <div class="container">
   <center>
