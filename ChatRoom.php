c0b8568ef8e82d3326ad791bcb98aeb7b4caea29
rbretmounet <rbretmounet@gmail.com>
2018-05-21 11:20:05 -0700

    Removed cookie generator form main php file since it caused conflicts.

--- ChatRoom.php
+++ ChatRoom.php
@@ -17,23 +17,6 @@
 </head>
 
 <body>
-  <?php
-  //Creates variable for $cookie_name called user
-  $cookie_name = "user";
-  //When there is a $cookie_name defined then it starts code.
-  if(!isset($_COOKIE[$cookie_name])) {
-    //creates a varialbe called url to random name generator api.
-    $url = "https://uinames.com/api/?ext/";
-    //creates a variable called obj to get the api.
-    $obj = json_decode(file_get_contents($url), true);
-    //sets $cookie_value to name of api
-    $cookie_value = $obj['password'];
-    setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 seconds = 1 day x 1 days - to stay anonymous
-  }else{
-    $cookie_value = $_COOKIE[$cookie_name];
-
-  }
-  ?>
   <div class="chatBox" id="ChatView">
     <iframe src="/ChatView.php" width=400 height=400 frameborder=0 id="innerframe"></iframe>
   </div>
