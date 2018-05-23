2e5f19a9b5c53f263172834c55b7adbaa21bd3d7
rbretmounet <rbretmounet@gmail.com>
2018-05-22 19:46:08 -0700

    changed random name generator api to star wars names.

--- ChatRoom.php
+++ ChatRoom.php
@@ -22,7 +22,7 @@
     //creates a variable called obj to get the api.
     $obj = json_decode(file_get_contents($url), true);
     //sets $cookie_value to name of api
-    $cookie_value = $obj['surname'];
+    $cookie_value = $obj['name'];
     setcookie($cookie_name, $cookie_value, "/"); //Sets username until session is expired
   }else{
     $cookie_value = $_COOKIE[$cookie_name];
