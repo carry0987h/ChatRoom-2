4f4711316846a0da5a9866b5e1d9b3afa21433ec
rbretmounet <rbretmounet@gmail.com>
2018-05-22 20:29:18 -0700

    made user names only random first names U.S. names.

--- ChatRoom.php
+++ ChatRoom.php
@@ -12,23 +12,6 @@
   <div class="title">
     <h1>ANON CHAT</h1>
   </div>
-  <?php
-  //Creates variable for $cookie_name called user
-  $cookie_name = "user";
-  //When there is a $cookie_name defined then it starts code.
-  if(!isset($_COOKIE[$cookie_name])) {
-    //creates a varialbe called url to random name generator api.
-    $url = "https://uinames.com/api/?region=united%20states";
-    //creates a variable called obj to get the api.
-    $obj = json_decode(file_get_contents($url), true);
-    //sets $cookie_value to name of api
-    $cookie_value = $obj['name'];
-    setcookie($cookie_name, $cookie_value, "/"); //Sets username until session is expired
-  }else{
-    $cookie_value = $_COOKIE[$cookie_name];
-
-  }
-  ?>
   <div class="chatBox" id="ChatView">
     <iframe src="/ChatView.php" width=400 height=400 frameborder=0 id="innerframe"></iframe>
   </div>
