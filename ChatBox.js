f742d21764f895a3235359768bacd1dd390bfd76
rbretmounet <rbretmounet@gmail.com>
2018-05-23 11:22:07 -0700

    removed function refresh from since it did nothing.

--- ChatBox.js
+++ ChatBox.js
@@ -4,21 +4,3 @@ function checkSubmit(e){
     document.forms[0].submit();
   }
 };
-
-function refresh()
-		{
-				var req = new XMLHttpRequest();
-				console.log("Grabbing Value");
-				req.onreadystatechange=function() {
-					if (req.readyState==4 && req.status==200) {
-						document.getElementById('trulyCodesFavouriteNumber').innerText = req.responseText;
-					}
-				}
-				req.open("GET", '/ChatView.php', true);
-				req.send(null);
-		}
-		function init()
-		{
-			refresh()
-			var int=self.setInterval(function(){refresh()},10000);
-		}
